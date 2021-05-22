e087d3e44107d10e52537e415055317aa73e8ae4
Ossamoon <osamu950623@gmail.com>
2021-05-22 18:42:06 +0900

    copy html and css

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container">
+        <div class="container__left">
+          <div class="frame-wrapper__video">
+            <iframe
+              width="560"
+              height="315"
+              src="https://www.youtube-nocookie.com/embed/vp_h649sZ9A"
+              title="YouTube video player"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </div>
+        </div>
+        <div class="resizer" id="dragMe"></div>
+        <div class="container__right">Right</div>
+      </div>
     </div>
   );
 }
